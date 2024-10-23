d5e8d277054e2b252665cf58266b01e5ad7d61a7
Nicolas <n.abuin2005@gmail.com>
2024-10-23 11:30:22 +0200

    Creacion de la clase Pelicula, basico, Actualizacion

--- models/Peliculas.cs
+++ models/Peliculas.cs
@@ -1,3 +1,17 @@
 namespace models;
 
-class Peliculas
+class Peliculas{
+
+    public string Nombre {get;set;}
+
+    public double Precio {get;set;}
+
+    public Peliculas(string nombre, double precio){ //Constructor
+        Nombre = nombre;
+        Precio = precio;
+
+        //Esto es lo mismo, que en java era this.Nombre = nombre
+
+    }
+    
+}
